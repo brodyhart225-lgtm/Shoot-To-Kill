f068fbd196dbe53be257c7ad198768e84657ba3d
Brody <brodyhart225@gmail.com>
2026-05-12 10:31:39 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-pygame.init()
 from game import *
-print(f'This game is currently in version {version}\nEnjoy!')
 if __name__ == '__main__':
+    print(f'This game is currently in version {version}\nEnjoy!')
+    pygame.init()
     main()
